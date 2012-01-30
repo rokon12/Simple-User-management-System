6186ed48b5a76e4b87f4f3dd4f764b3f59ef2458
Bazlur Rahman Rokon <anm_brr@live.com>
2012-01-30 12:36:01 +0600

    relationship mapping update

--- UserManagement/src/com/rokonoid/apps/user/domain/Role.java
+++ UserManagement/src/com/rokonoid/apps/user/domain/Role.java
@@ -1,11 +1,19 @@
 package com.rokonoid.apps.user.domain;
 
 import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -24,13 +32,13 @@ public class Role {
 	@Column(name = "deleted")
 	private boolean deleted;
 
-	// @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-	// @JoinColumn(name = "permissionId", insertable = true, updatable = true)
-	// private List<Permission> permissions;
-	//
-	// @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-	// @JoinColumn(name = "userId", insertable = true, updatable = true)
-	// private List<User> users;
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name = "ROLE_PERMISSION", joinColumns = { @JoinColumn(name = "role_id") }, inverseJoinColumns = { @JoinColumn(name = "permission_id") })
+	private List<Permission> permissions;
+
+	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name = "ROLE_USER", joinColumns = { @JoinColumn(name = "role_id") }, inverseJoinColumns = { @JoinColumn(name = "user_id") })
+	private List<User> users;
 
 	public String getName() {
 		return name;
@@ -70,6 +78,22 @@ public class Role {
 
 	public void setDeleted(boolean deleted) {
 		this.deleted = deleted;
+	}
+
+	public List<Permission> getPermissions() {
+		return permissions;
+	}
+
+	public void setPermissions(List<Permission> permissions) {
+		this.permissions = permissions;
+	}
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
 	}
 
 	// public List<Permission> getPermissions() {
